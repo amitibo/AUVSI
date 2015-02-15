5359156ef28b5e51e4c507cf8e982b1b5e533ab7
amitibo <amitibo@localhost>
2015-02-15 22:35:33 +0200

    First version that downloads images from airborne computer.

--- ground/AUVSI/server.py
+++ ground/AUVSI/server.py
@@ -1,7 +1,10 @@
-from twisted.internet import reactor, protocol, task
+from twisted.internet import reactor, protocol, task, threads
 from twisted.web.client import getPage
 from twisted.enterprise import adbapi
+import global_settings as gs
 from datetime import datetime
+import urllib
+import json
 import os
 
 
@@ -42,10 +45,7 @@ class ServerFactory(protocol.ReconnectingClientFactory):
     
     def __init__(
         self,
-        app,
-        database_path='~/auvsi_db',
-        images_db='images.db',
-        images_table='images_table'
+        app
         ):
         
         self.app = app
@@ -53,12 +53,14 @@ class ServerFactory(protocol.ReconnectingClientFactory):
         #
         # Initialize database stuff
         #
-        self.database_path = os.path.abspath(os.path.expanduser(database_path))
-        self.images_db = os.path.join(self.database_path, images_db)
-        self.images_table = images_table
+        self.database_path = gs.DB_FOLDER
+        self.images_db = gs.DB_PATH
+        self.images_table = gs.IMAGES_TABLE
     
         if not os.path.exists(self.database_path):
             os.makedirs(self.database_path)
+        if not os.path.exists(gs.IMAGES_FOLDER):
+            os.makedirs(gs.IMAGES_FOLDER)
     
         self.dbpool = adbapi.ConnectionPool("sqlite3", self.images_db)        
 
@@ -94,17 +96,61 @@ class ServerFactory(protocol.ReconnectingClientFactory):
         self.app.print_message("connection failed")
         self.retry(conn)
 
-    def _downloadNewImages(self, entries_list):
+    def _dbNewImg(self, data):
         
-        for entry in entries_list:
-            print entry
+        img_path, new_imgs = data
+        
+        new_img = new_imgs.pop(0)
+        cmd = "INSERT INTO {table_name}(image_path, timestamp) values (?, ?)".format(table_name=self.images_table)
+        d = self._db_cmd(cmd, (img_path, new_img['timestamp']))
+        
+        return new_imgs
     
+    def _downloadNewImg(self, new_imgs):
+        
+        new_img = new_imgs[0]
+        img_url = 'http://{ip}:{port}/images/{img}'.format(ip=_server_address['ip'], port=_server_address['port'], img=new_img['name'])
+        img_path = os.path.join(gs.IMAGES_FOLDER, new_img['name'])
+        
+        print 'Downloading:', img_url, 'to', img_path
+        
+        urllib.urlretrieve(img_url, img_path)
+        
+        return img_path, new_imgs
+    
+    def _loopNewImgs(self, new_imgs):
+        if len(new_imgs) == 0:
+            self.images_task.start(1)
+            return
+        print len(new_imgs), 'images more to go.'
+        d = threads.deferToThread(self._downloadNewImg, new_imgs)
+        d.addCallback(self._dbNewImg)
+        d.addCallback(self._loopNewImgs)        
+    
+    def _setupNewImagesLoop(self, entries_list):
+        
+        entries_list = json.loads(entries_list)
+        
+        if len(entries_list) == 0:
+            return
+        
+        #
+        # Stop checking for new images till all images downloaded.
+        #
+        self.images_task.stop()
+        
+        new_imgs = [{'name': os.path.split(entry[1])[1], 'timestamp': entry[2]} for entry in entries_list]
+        print 'Setting up:', len(new_imgs), 'new images'
+        self._loopNewImgs(new_imgs)
+        
     def _lastTimeStamp(self, timestamp):
         
         if timestamp == []:
             timestamp = ''
-    
-        d = access('new_imgs='+timestamp, self._downloadNewImages)
+        else:
+            timestamp=timestamp[0][0].replace(' ', 'T')
+            
+        d = access('new_imgs='+timestamp, self._setupNewImagesLoop)
     
     def updateImagesDB(self):
 
@@ -126,9 +172,10 @@ def connect(app, server):
     global _server_address
     _server_address = server
 
+
 def access(page, callback=printPage):
-    d = getPage(
-        'http://{ip}:{port}/{page}'.format(ip=_server_address['ip'], port=_server['port'], page=page)
-    )
+    url = 'http://{ip}:{port}/{page}'.format(ip=_server_address['ip'], port=_server_address['port'], page=page)
+    print 'Accessing url:', url
+    d = getPage(url)
     d.addCallbacks(callback, printError)
     return d
