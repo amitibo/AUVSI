1fbe38ce3fead2e7b0b440a9adc2b4ec212641e3
Ori Ashur <ori155+st@gmail.com>
2015-03-21 19:04:57 +0200

    low_resulotion service can synchronize a directory to the ftp server.

--- airborne/AUVSIairborne/services/low_resulotion.py
+++ airborne/AUVSIairborne/services/low_resulotion.py
@@ -5,7 +5,10 @@ and send those new files downstream.
 """
 
 from twisted.internet import task
-from twisted.protocols.ftp import FTPClient, FTPFileListProtocol
+from twisted.protocols.ftp import FTPClient
+from twisted.python import log
+import AUVSIairborne.global_settings as settings
+import os
 
 
 def success(response):
@@ -23,30 +26,66 @@ def fail(error):
     print error
 
 
-def send_image(consumer):
-    with open(r'C:\Users\Ori\Pictures\hot-girls.jpg', 'rb') as image:
+class FileSendingScheduler(object):
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+        self.files_sent = []
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        result = self.next_file()
+        if result is None:
+            raise StopIteration()
+
+        return result
+
+    def next_file(self):
+        for file_name in os.listdir(self.dir_path):
+            if file_name not in self.files_sent:
+                self.files_sent.append(file_name)
+                return file_name
+        return None
+
+
+def send_image(consumer, path):
+    log.msg("send_image({})".format(path))
+    with open(path, 'rb') as image:
         for line in image.read():
             consumer.write(line)
     consumer.finish()
 
 
-def sync_images(ftp_client):
-    ftp_client.pwd().addCallbacks(success, fail)
+def sync_images(ftp_client, scheduler):
+    log.msg("sync_mages")
+    for image in scheduler:
+        consumer_defer, control_defer = ftp_client.storeFile(image)
+        image_path = os.path.join(scheduler.dir_path, image)
+        consumer_defer.addCallback(send_image, path=image_path)
 
-    consumer_defer, control_defer = ftp_client.storeFile('coolImage.jpg')
-    consumer_defer.addCallbacks(send_image, fail)
+
+def start_sync(ftp_client, scheduler):
+    log.msg("Starting sync")
+    sync_task = task.LoopingCall(sync_images, ftp_client=ftp_client,
+                                 scheduler=scheduler)
+    sync_task.start(1)
+
 
 
 if __name__ == "__main__":
     from twisted.internet import reactor
     from twisted.internet.protocol import ClientCreator
+    from sys import stdout
 
-    # def dummy():
-    #     print "HODOR!!!"
-    #
-    # task.LoopingCall(dummy).start(0.5)
+    log.startLogging(stdout)
+
+    image_scheduler = FileSendingScheduler(r'C:\Users\Ori\Pictures')
 
     creator = ClientCreator(reactor, FTPClient, 'Auvsi',
                             '1234', passive=0)
-    creator.connectTCP('localhost', 21).addCallbacks(sync_images, fail)
+
+    connected = creator.connectTCP('localhost', 21)
+
+    connected.addCallback(start_sync, scheduler=image_scheduler)
     reactor.run()
