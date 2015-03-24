2ddeba8bc13cf742281d9f9852c034987edc3e72
Ori Ashur <ori155+st@gmail.com>
2015-03-24 22:06:43 +0200

    Experiment fix patch

--- airborne/scripts/start_auvsi.py
+++ airborne/scripts/start_auvsi.py
@@ -1,18 +1,37 @@
 from __future__ import division
-from AUVSIairborne import start_server
 import argparse
-
-
-def main(camera_type, port):
-    
-    start_server(camera_type=camera_type, port=port)
-
+from AUVSIairborne.camera import CameraController, CanonCamera
+from AUVSIairborne.services.system_control import SystemControlFactory
+from twisted.python import log
+from sys import stdout
+from twisted.internet import reactor
+from AUVSIairborne.services.directory_synchronization_ftp import \
+    DirSyncClientFactory
+import AUVSIairborne.global_settings as settings
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Start the airborne server.')
-    parser.add_argument('--camera', type=str, default='canon', help='Camera type. Options: [canon (default), simlulation].')
-    parser.add_argument('--port', type=int, default=8000, help='Server port (default=8000).')
+    parser = argparse.ArgumentParser(description="Start the airborne server"
+                                                 " - Oris' version.")
+
+    parser.add_argument('--port', type=int, default=8844,
+                        help='Control port(default=8844).')
+    parser.add_argument('--gs_ip', type=str,
+                        help='Ground station IP')
     args = parser.parse_args()
-    
-    main(camera_type=args.camera, port=args.port)
-    
+
+    camera_controller = CameraController(CanonCamera())
+
+    control_factory = SystemControlFactory()
+    control_factory.subscribe_subsystem("camera", camera_controller.apply_cmd)
+
+    image_sending = DirSyncClientFactory(
+        dir_to_sync=settings.IMAGES_FOLDER,
+        sync_interval=1,
+        ftp_user='auvsi',
+        ftp_pass='1234'
+    )
+
+    log.startLogging(stdout)
+    reactor.listenTCP(args.port, control_factory)
+    reactor.connectTCP(args.gs_ip, 21, image_sending)
+    reactor.run()
