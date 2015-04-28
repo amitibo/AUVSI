c4f78dbcee536078221462e42a2ebdd03e99d93e
amitibo <amitibo@localhost>
2015-04-28 09:42:01 +0300

    Implemented a basic train_patches code. Too many false positives.

--- image_processing/scripts/train_patches.py
+++ image_processing/scripts/train_patches.py
@@ -8,6 +8,7 @@ import cv2
 import glob
 import os
 import time
+import pickle
 
 VISUALIZE = False
 
@@ -16,15 +17,62 @@ def learn():
     samples = np.load(os.path.join(base_path, 'patch_samples.npy'))
     labels = np.load(os.path.join(base_path, 'patch_labels.npy'))
 
+    print len(labels)
+    
     kf = cross_validation.StratifiedKFold(labels, n_folds=4, shuffle=True)
     
     svc = svm.SVC(kernel='linear')
     
-    for train_index, test_index in kf:
-        svc.fit(samples[train_index], labels[train_index])
+    for c in range(3):
+        for train_index, test_index in kf:
+            svc.fit(samples[c][train_index], labels[train_index])
+            
+            print c, svc.score(samples[c][test_index], labels[test_index])
+            
+
+def learn_save():
+    base_path = os.environ['AUVSI_CV_DATA']
+    samples = np.load(os.path.join(base_path, 'patch_samples.npy'))
+    labels = np.load(os.path.join(base_path, 'patch_labels.npy'))
+
+    print 'learning classifier'
+    svc = svm.SVC(kernel='linear')
+    svc.fit(samples[0], labels)
+    
+    print 'saving classifier'
+    with open(os.path.join(base_path, 'linear_svc.pkl'), 'wb') as f:
+        s = pickle.dump(svc, f)
         
-        print svc.score(samples[test_index], labels[test_index])
+
+def test_classifier():
+    base_path = os.environ['AUVSI_CV_DATA']
+    imgs_paths = sorted(glob.glob(os.path.join(base_path, 'test', '*.jpg')))
+    data_paths = [os.path.splitext(path)[0]+'.json' for path in imgs_paths]
+    
+    with open(os.path.join(base_path, 'linear_svc.pkl'), 'rb') as f:
+        clf = pickle.load(f)
+    
+    #
+    # Load image and image data
+    #
+    for img_path, data_path in zip(imgs_paths, data_paths):
+        print 'Extracting patches from image', img_path
         
+        img = AUVSIcv.Image(img_path, data_path)
+        
+        patches = img.createPatches(patch_size=(100, 100), patch_shift=20)
+            
+        for patch in patches:
+            patch_gray = cv2.cvtColor(patch, cv2.cv.CV_RGB2HSV)[..., 0]
+            fd = hog(patch_gray, orientations=8, pixels_per_cell=(16, 16),
+                            cells_per_block=(1, 1), visualise=False)
+            
+            if clf.predict(fd)[0] == 1:
+                cv2.namedWindow('image', flags=cv2.WINDOW_NORMAL)
+                cv2.imshow('image', patch)
+                cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
 
 def extract():
 
@@ -36,15 +84,15 @@ def extract():
     # Load image and image data
     #
     with_target_flag = 0
-    samples = []
+    samples = [[], [], []]
     labels = []
     
-    for img_path, data_path in zip(imgs_paths[:20], data_paths[:20]):
+    for img_path, data_path in zip(imgs_paths, data_paths):
         print 'Extracting patches from image', img_path
         
         img = AUVSIcv.Image(img_path, data_path)
         
-        patches = img.createPatches(patch_size=(100, 100), patch_shift=50)
+        patches = img.createPatches(patch_size=(100, 100), patch_shift=20)
             
         for patch in patches:
             target = AUVSIcv.randomTarget(
@@ -56,18 +104,19 @@ def extract():
             if with_target_flag:            
                 img.pastePatch(patch=patch, target=target)
 
-            patch_gray = cv2.cvtColor(patch, cv2.cv.CV_RGB2HSV)[..., 2]
-            fd = hog(patch_gray, orientations=8, pixels_per_cell=(16, 16),
+            for c in range(3):
+                patch_gray = cv2.cvtColor(patch, cv2.cv.CV_RGB2HSV)[..., c]
+                fd = hog(patch_gray, orientations=8, pixels_per_cell=(16, 16),
                                 cells_per_block=(1, 1), visualise=False)
+                samples[c].append(fd)
             
-            if VISUALIZE:
-                hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
-                
-                cv2.namedWindow('image', flags=cv2.WINDOW_NORMAL)
-                cv2.imshow('image', hog_image_rescaled)
-                cv2.waitKey(0)
+                if VISUALIZE:
+                    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+                    
+                    cv2.namedWindow('image', flags=cv2.WINDOW_NORMAL)
+                    cv2.imshow('image', hog_image_rescaled)
+                    cv2.waitKey(0)
     
-            samples.append(fd)
             labels.append(with_target_flag)
             
             with_target_flag = 1 - with_target_flag
@@ -83,5 +132,6 @@ def extract():
 
 
 if __name__ == '__main__':
-    extract()
-    learn()
+    #extract()
+    #learn_save()
+    test_classifier()
