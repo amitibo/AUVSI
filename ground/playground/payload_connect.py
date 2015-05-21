202d81f41e2de2a952599a7561a66524f123ed35
amitibo <amitibo@localhost>
2015-05-21 09:04:35 +0300

    quick connect fix

---
+++ ground/playground/payload_connect.py
@@ -0,0 +1,13 @@
+__author__ = 'User'
+import telnetlib
+
+tel = telnetlib.Telnet(host="192.168.1.100",
+                       port=8855)
+delimiter = b'\r\n'
+cmd_list = ("resize_download connect ip 192.168.1.100",
+            "data_download connect ip 192.168.1.100",
+            "camera set shutter 1000 aperture 3 ISO 100")
+
+for cmd in cmd_list:
+    tel.write(cmd + delimiter)
+    tel.read_until("More")
