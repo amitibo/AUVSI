bceb8bb29307eedfdd85d39d27cfc7b7c1d283de
amitibo <amitibo@localhost>
2015-05-21 03:35:30 +0300

    created the function to convert lonlat from decimal to minsec

---
+++ ground/AUVSIground/utils.py
@@ -0,0 +1,30 @@
+__author__ = 'Ori'
+
+def decimal_to_minsec(*decimal):
+    """
+    Converts a number from decimal representation to minute-second
+    representation
+    :param decimal: the number in decimal (can be ether string or float)
+    :return: string
+    """
+    res = list()
+    for num in decimal:
+        num = float(num)
+        deg = int(num)
+        residue = num - deg
+
+        minutes = residue*60
+        residue = minutes - int(minutes)
+        minutes = int(minutes)
+
+        seconds = round(residue*60, 3)
+
+        deg_symbol = '\xB0'
+        degmin = '''{deg}{deg_sym}{minutes}'{sec}"'''.format(deg=deg,
+                                                       deg_sym=deg_symbol,
+                                                       minutes=minutes,
+                                                       sec=seconds)
+
+        res.append(degmin)
+
+    return res
